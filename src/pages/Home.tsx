a812ce9b1af170cff482ae99a5b7d6c32c6b634d
Amrit Bastakoti <amritbastakoti618@gmail.com>
2022-11-20 11:30:01 +0200

    added some styles to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,28 +1,30 @@
 import Box from "@mui/material/Box";
-import React from "react";
 import Footer from "../components/Footer";
 import { useAppSelector } from "../hooks/appHook";
+import "react-slideshow-image/dist/styles.css";
+import { Slide } from 'react-slideshow-image';
 
 const Home = () => {
   const { productList } = useAppSelector((state) => state.productReducer);
 
   return (
-    <Box className="images_container">
-      <div>
+    <div className="container">
+    <Slide>
+      <div className="title">
         <h1>Warm Welcome to our FakeShopping</h1>
       </div>
       {productList.map((sortitems: any) => (
         <div key={sortitems.id}>
-          <img
-            className="image_product"
+          <img 
+            className="image_home"
             src={sortitems.images[0]}
             alt="imagespic"
           />
         </div>
       ))}
       <Footer/>
-    </Box>
-    
+    </Slide>
+    </div>
   );
 };
 export default Home;
