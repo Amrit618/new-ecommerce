3b0923eb1e6a8ecccf9fe7469b2a857ab0f79641
Amrit Bastakoti <amritbastakoti618@gmail.com>
2022-11-25 19:47:34 +0200

    summary page for products added to cart

---
+++ src/pages/Summary.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components"
+
+const Container = styled.div`
+padding: 20px;
+height: 100px;
+width: 100%;
+`
+
+const Summary = () => {
+  return (
+    <Container>Summary</Container>
+  )
+}
+
+export default Summary
