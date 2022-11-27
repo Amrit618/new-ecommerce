7950d6374e50e505f2705773e315632b4a1de3a8
Amrit Bastakoti <amritbastakoti618@gmail.com>
2022-11-27 20:52:49 +0200

    newsletter for notified products

--- src/components/Newsletter.tsx
+++ src/components/Newsletter.tsx
@@ -2,10 +2,33 @@ import styled from "styled-components";
 
 const Container = styled.div`
   display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: black;
+  min-width: 1024px;
+  min-height: 100px;
 `;
+const Form = styled.div`
+display:flex;
+align-items: center;
+justify-content: center;
+padding: 50px;
+`
+
+const Title = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+color: white;
+`
 
 const Newsletter = () => {
-  return <Container>Get Notified about our awesome Products</Container>;
+  return <Container>
+    <Title>Get notifiedd about our trending products </Title>
+    <Form>
+    <input placeholder=" your mail" style={{padding: '20px', width: '300px'}}/>
+    </Form>
+    </Container>;
 };
 
 export default Newsletter;
