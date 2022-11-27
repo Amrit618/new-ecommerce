9c8897427eb7c7bababa7fd4b371e7aa9f780f1d
Amrit Bastakoti <amritbastakoti618@gmail.com>
2022-11-27 21:06:57 +0200

    new changes on cart item

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,14 +1,16 @@
-import { Card, CardMedia } from "@mui/material";
-import React from "react";
+import {  CardMedia } from "@mui/material";
 import { useNavigate } from "react-router-dom";
-import { useAppDispatch, useAppSelector } from "../hooks/appHook";
-import { addToCart } from "../redux/reducers/cartReducer";
-import { ProductInCart } from "../types/Products";
-import Summary from "./Summary";
+import styled from "styled-components";
+import {  useAppSelector } from "../hooks/appHook";
+
 const Cart = () => {
   const { products } = useAppSelector((state) => state.cartReducer);
   const navigate = useNavigate();
-  const dispatch = useAppDispatch();
+  const AddItems = styled.div`
+  height: 30px;
+  width: 20px;`
+  const MinusItems = styled.div``
+
   return (
     <div className="cartItem">
       {products.length > 0 &&
@@ -20,7 +22,10 @@ const Cart = () => {
               alt="Itemsincart"
               className="cartItem-card-image"
               onClick={() => navigate(`/products/${cartItem.id}`)}
+              
             ></CardMedia>
+            <AddItems><button>+</button></AddItems>
+            <MinusItems><button>-</button></MinusItems>
             <div>Price:{cartItem.price}</div>
             <div><button>Checkout</button></div>
           </div>
