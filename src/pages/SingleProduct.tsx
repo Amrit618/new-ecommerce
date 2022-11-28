e756805c47cacb974d6defdb9a2020d716ff429b
Amrit Bastakoti <amritbastakoti618@gmail.com>
2022-11-28 20:26:54 +0200

    single product managed

--- src/pages/SingleProduct.tsx
+++ src/pages/SingleProduct.tsx
@@ -4,14 +4,14 @@ import styled from "styled-components";
 import { ProductInCart } from "../types/Products";
 import { addToCart } from "../redux/reducers/cartReducer";
 
-
 const Container = styled.div`
-text-align: center
-`
+  text-align: center;
+  align-items: center;
+  justify-content:center:
+`;
 const SingleProduct = () => {
   const { productId } = useParams();
-  const product = useProduct(productId);  
-
+  const product = useProduct(productId);
 
   return (
     <Container>
@@ -19,7 +19,7 @@ const SingleProduct = () => {
         <div>
           <h3>Product Id: {product.id}</h3>
           <h3>Product Name: {product.title}</h3>
-          <img src={product.images[0]}/> 
+          <img src={product.images[0]} />
           <p>{product.description}</p>
           <h4>Price:{product.price}</h4>
           <button>Add to cart</button>
@@ -35,4 +35,3 @@ export default SingleProduct;
 function dispatch(arg0: any) {
   throw new Error("Function not implemented.");
 }
-
